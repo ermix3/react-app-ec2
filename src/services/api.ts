2cb16d06d0763f23eebd4ede60b145daa3d0be8e
Er. Reda <redaermich@gmail.com>
2025-04-23 00:54:36 +0100

    refactor: update API base URL and clean up code formatting in api.ts and ci.yml

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,40 +1,42 @@
 import axios from "axios";
-import { Product, ProductFormData } from "../types";
+import {Product, ProductFormData} from "../types";
 
-const API_BASE_URL = "http://localhost:8080/api/v1";
+// const BACKEND_CONATINER_NAME = "sb-app-ec";
+const IP_BACKEND = "13.60.2.140";
+const API_BASE_URL = `http://${IP_BACKEND}:8080/api/v1`;
 
 const apiClient = axios.create({
-  baseURL: API_BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+    baseURL: API_BASE_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 export const productAPI = {
-  getProducts: async (): Promise<Product[]> => {
-    const response = await apiClient.get("/products");
-    return response.data;
-  },
+    getProducts: async (): Promise<Product[]> => {
+        const response = await apiClient.get("/products");
+        return response.data;
+    },
 
-  getProductById: async (id: number): Promise<Product> => {
-    const response = await apiClient.get(`/products/${id}`);
-    return response.data;
-  },
+    getProductById: async (id: number): Promise<Product> => {
+        const response = await apiClient.get(`/products/${id}`);
+        return response.data;
+    },
 
-  createProduct: async (product: ProductFormData): Promise<Product> => {
-    const response = await apiClient.post("/products", product);
-    return response.data;
-  },
+    createProduct: async (product: ProductFormData): Promise<Product> => {
+        const response = await apiClient.post("/products", product);
+        return response.data;
+    },
 
-  updateProduct: async (
-    id: number,
-    product: ProductFormData
-  ): Promise<Product> => {
-    const response = await apiClient.put(`/products/${id}`, product);
-    return response.data;
-  },
+    updateProduct: async (
+        id: number,
+        product: ProductFormData
+    ): Promise<Product> => {
+        const response = await apiClient.put(`/products/${id}`, product);
+        return response.data;
+    },
 
-  deleteProduct: async (id: number): Promise<void> => {
-    await apiClient.delete(`/products/${id}`);
-  },
+    deleteProduct: async (id: number): Promise<void> => {
+        await apiClient.delete(`/products/${id}`);
+    },
 };
